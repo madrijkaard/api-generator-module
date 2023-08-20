97d804ca24596446c81668f4cafe002ff9599b5a
madrijkaard <mad.rijkaard@gmail.com>
2023-08-20 00:51:19 -0300

    Initial commit.

--- create-project.py
+++ create-project.py
@@ -9,76 +9,22 @@ def create_directories(path):
     except FileExistsError:
         print(f"Directory already exists: {path}")
 
-def create_pom_xml(project_directory, spring_boot_starter_parent_version, group_id, artifact_id, project_name, java_version):
-    pom_template = """\
-<project xmlns="http://maven.apache.org/POM/4.0.0"
-         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-         
-    <modelVersion>4.0.0</modelVersion>
-    
-    <parent>
-        <groupId>org.springframework.boot</groupId>
-        <artifactId>spring-boot-starter-parent</artifactId>
-        <version>{spring_boot_starter_parent_version}</version>
-        <relativePath/>
-    </parent>
-    
-    <groupId>{group_id}</groupId>
-    <artifactId>{artifact_id}</artifactId>
-    <version>0.0.1-SNAPSHOT</version>
-    <name>{project_name}</name>
+def create_pom_xml(project_directory, pom_template_path, spring_boot_starter_parent_version, group_id, artifact_id, project_name, java_version):
+    with open(pom_template_path, "r") as template_file:
+        pom_template = template_file.read()
 
-    <properties>
-		<java.version>{java_version}</java.version>
-	</properties>
-    
-    <dependencies>
-		<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter</artifactId>
-		</dependency>
+    pom_content = pom_template.replace(
+        "{spring_boot_starter_parent_version}", spring_boot_starter_parent_version
+    ).replace(
+        "{group_id}", group_id
+    ).replace(
+        "{artifact_id}", artifact_id
+    ).replace(
+        "{project_name}", project_name
+    ).replace(
+        "{java_version}", str(java_version)
+    )
 
-		<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter-web</artifactId>
-		</dependency>
-
-		<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter-validation</artifactId>
-		</dependency>
-
-		<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter-data-cassandra</artifactId>
-		</dependency>
-
-		<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter-test</artifactId>
-			<scope>test</scope>
-		</dependency>
-    </dependencies>
-
-	<build>
-		<plugins>
-			<plugin>
-				<groupId>org.springframework.boot</groupId>
-				<artifactId>spring-boot-maven-plugin</artifactId>
-			</plugin>
-		</plugins>
-	</build>
-    
-</project>
-"""
-
-    pom_content = pom_template.format(spring_boot_starter_parent_version=spring_boot_starter_parent_version, 
-                                      group_id=group_id, 
-                                      artifact_id=artifact_id,
-                                      project_name=project_name,
-                                      java_version=java_version)
-    
     pom_path = os.path.join(project_directory, "pom.xml")
 
     with open(pom_path, "w") as pom_file:
@@ -108,12 +54,20 @@ public class {class_name} {{
         java_file.write(java_content)
         print(f"Created {class_name}.java")
 
+def create_application_properties(directory):
+    application_properties_content = ""
+
+    properties_path = os.path.join(directory, "application.properties")
+
+    with open(properties_path, "w") as properties_file:
+        properties_file.write(application_properties_content)
+        print(f"Created application.properties in {directory}")
+
 def main():
     root_directory = os.getcwd()
     config_directory = os.path.join(root_directory, "config")
     out_directory = os.path.join(root_directory, "out")
 
-    # Ler o arquivo main.json no diretório config
     config_path = os.path.join(config_directory, "main.json")
     with open(config_path, "r") as config_file:
         config_data = json.load(config_file)
@@ -124,15 +78,14 @@ def main():
         main_package = config_data["main-package"]
         java_version = config_data["java-version"]
 
-    # Criar o diretório com o mesmo nome do project-name no diretório out
+    pom_template_path = os.path.join(config_directory, "maven", "main.xml")
+
     project_directory = os.path.join(out_directory, project_name)
     create_directories(project_directory)
 
-    # Criar a sequência de diretórios em project_directory
     for dir_name in ["src/main/java", "src/main/resources", "src/test/java", "src/test/resources"]:
         create_directories(os.path.join(project_directory, dir_name))
 
-    # Criar a estrutura de pacotes em src/main/java e src/test/java
     main_java_path = os.path.join(project_directory, "src/main/java")
     test_java_path = os.path.join(project_directory, "src/test/java")
 
@@ -142,13 +95,22 @@ def main():
         create_directories(main_java_path)
         create_directories(test_java_path)
 
-    # Criar o arquivo pom.xml dentro do diretório do projeto
-    create_pom_xml(project_directory, spring_boot_starter_parent_version, group_id, artifact_id, project_name, java_version)
+    create_pom_xml(
+        project_directory,
+        pom_template_path,
+        spring_boot_starter_parent_version,
+        group_id,
+        artifact_id,
+        project_name,
+        java_version
+    )
 
-    # Criar o arquivo .java no diretório main_java_path
     artifact_camel_case = "".join(word.capitalize() for word in artifact_id.split("-"))
     class_name = f"{artifact_camel_case}Application"
     create_java_file(main_java_path, class_name, main_package)
+
+    create_application_properties(os.path.join(project_directory, "src/main/resources"))
+    create_application_properties(os.path.join(project_directory, "src/test/resources"))
 
 if __name__ == "__main__":
     main()
